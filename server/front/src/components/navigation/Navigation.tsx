f02d75aadedebda5c805c41170c52fb712d9f1dd
arkiix <arkiix@DESKTOP-AKUE4SF>
2024-03-15 01:59:19 +0600

    Changed the client download button

--- server/front/src/components/navigation/Navigation.tsx
+++ server/front/src/components/navigation/Navigation.tsx
@@ -5,7 +5,7 @@ import logo from '../../assets/navigation/logo.svg'
 import styles from "./Navigation.module.css"
 import { useNavigate } from 'react-router-dom';
 import clientService from '../../api/clientService';
-
+import {ReactComponent as DownloadIcon} from '../../assets/navigation/download.svg';
 
 interface NavigationProps {
     numPage: number;
@@ -40,7 +40,10 @@ function Navigation({ numPage }: NavigationProps) {
                                 <li><a href="/teams" className={numPage === 2 ? styles.nav__page_active : styles.nav__page}>Teams</a></li>
                                 <li><a href="/settings" className={numPage === 3 ? styles.nav__page_active : styles.nav__page}>Settings</a></li>                       
                             </ul>
-                            <button className={styles.client__button} onClick={downloadClient}>Download client</button>
+                            <button className={styles.client__button} onClick={downloadClient}>
+                                <DownloadIcon />
+                                <div>Client</div>
+                            </button>
                             <button className={styles.logout__button} onClick={onLogout}>Logout</button>
                         </div>
                     }
